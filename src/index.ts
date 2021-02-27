ebdad7a87dad53752698bdd30b639cd9a946e2ad
Carlson Santana Cruz <carlsonsantana@gmail.com>
2021-02-27 18:05:54 -0300

    Allow build the project using TypeScript

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import {
+    Character as CharacterImport,
+    Command as CommandImport,
+    StateEntry as StateEntryImport
+} from './character';
+
+import {Control as ControlImport} from './control';
+
+export const Character = CharacterImport;
+export const Command = CommandImport;
+export const StateEntry = StateEntryImport;
+export const Control = ControlImport;
