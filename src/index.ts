2d8e5fe28eca00ec39c7c1a52e3d196f84a6ce87
Carlson Santana Cruz <carlsonsantana@gmail.com>
2021-02-27 19:00:53 -0300

    Fix exported classes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,2 @@
-import {
-    Character as CharacterImport,
-    Command as CommandImport,
-    StateEntry as StateEntryImport
-} from './character';
-
-import {Control as ControlImport} from './control';
-
-export const Character = CharacterImport;
-export const Command = CommandImport;
-export const StateEntry = StateEntryImport;
-export const Control = ControlImport;
+export {Character, Command, StateEntry} from './character';
+export {Control} from './control';
